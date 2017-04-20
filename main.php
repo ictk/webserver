3ed4a5719e14a0e4156fc24720e1ba90c40585d5
neo1seok <neo1seok@gmail.com>
2017-04-20 17:15:55 +0900

    aa

--- main.php
+++ main.php
@@ -1,22 +1,38 @@
-<ul>
-  <li><a href="http://neo1seok.pe.kr">neo1seok.pe.kr</a></li>
-  <li><a href="giant_se/">giant_se</a></li>
-  <li><a href="giant_se/auth.do">auth</a></li>
-  <li><a href="sim_reg.html">giant_se sim reg</a></li>
-  <li><a href="rd/nnbaboojmfbganncopiejdhpgnagciek">reirecting test</a></li>
-  <li><a href="http://testmain.com">testmain.com</a></li>
-  <li><a href="test.html">테스트페이지</a></li>
-  <li><a href="http://neo.test.com">neo.test.com</a></li>
-  <li><a href="http://neo.test1.com">neo.test1.com</a></li>
-  <li><a href="http://neo.test2.com">neo.test2.com</a></li>
-  <li><a href="http://neo.test3.com">neo.test3.com</a></li>
-  <li><a href="http://neo.webtoon.com">neo.webtoon.com</a></li>
-  <li><a href="http://g2aiha1z-a.com">g2aiha1z-a.com</a></li>
-  <li><a href="http://g2aiha1z-b.com">g2aiha1z-b.com</a></li>
-  <li><a href="http://g2aiha1z-c.com">g2aiha1z-c.com</a></li>
-  <li><a href="http://g2aiha1z-a.com/abcdef1234">g2aiha1z-a.com whth sn</a></li>
-  <li><a href="http://g2aiha1z-b.com/abcdef1234">g2aiha1z-b.com whth sn</a></li>
-  <li><a href="http://g2aiha1z-c.com/abcdef1234">g2aiha1z-c.com whth sn</a></li>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Bootstrap Example</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet"
+	href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<script
+	src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+<script
+	src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+  <ul>
+    <li><a href="http://neo1seok.pe.kr">neo1seok.pe.kr</a></li>
+    <li><a href="giant_se/">giant_se</a></li>
+    <li><a href="giant_se/auth.do">auth</a></li>
+    <li><a href="sim_reg.html">giant_se sim reg</a></li>
+    <li><a href="rd/nnbaboojmfbganncopiejdhpgnagciek">reirecting test</a></li>
+    <li><a href="http://testmain.com">testmain.com</a></li>
+    <li><a href="test.html">테스트페이지</a></li>
+    <li><a href="http://neo.test.com">neo.test.com</a></li>
+    <li><a href="http://neo.test1.com">neo.test1.com</a></li>
+    <li><a href="http://neo.test2.com">neo.test2.com</a></li>
+    <li><a href="http://neo.test3.com">neo.test3.com</a></li>
+    <li><a href="http://neo.webtoon.com">neo.webtoon.com</a></li>
+    <li><a href="http://g2aiha1z-a.com">g2aiha1z-a.com</a></li>
+    <li><a href="http://g2aiha1z-b.com">g2aiha1z-b.com</a></li>
+    <li><a href="http://g2aiha1z-c.com">g2aiha1z-c.com</a></li>
+    <li><a href="http://g2aiha1z-a.com/abcdef1234">g2aiha1z-a.com whth sn</a></li>
+    <li><a href="http://g2aiha1z-b.com/abcdef1234">g2aiha1z-b.com whth sn</a></li>
+    <li><a href="http://g2aiha1z-c.com/abcdef1234">g2aiha1z-c.com whth sn</a></li>
 
 
-</ul>
+  </ul>
+
+</body>
