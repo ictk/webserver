fec97a844f45cd5b1160c71cc2f539cac37071f2
Ubuntu <ubuntu@dev.ictk.com>
2017-04-20 06:48:39 +0000

    a

--- js/util.js
+++ js/util.js
@@ -1,4 +1,8 @@
 
+	function test() {
+		console.log('test');
+
+	}
 		var def_function=function(){};
 		function btn_templtate(selecter, sublet_address, json_function,
 				succes_function) {
@@ -35,25 +39,29 @@
 
 		};
     function make_form(selector,input_info) {
-
+      var input_unit_form = '<label for="{0}">{1}</label>     <input class="form-control  input-lg"		id="{0}" type="text">\n';
+      var str_form = '<div class="form-group" id={0}>\n{1}   \n <button type="button" id="{2}" class="btn btn-lg btn-primary">{3}</button> </div><br/><br/>';
+      //0:group_id 1:input_str 2:id 3:lable
 
 
         $.each(input_info, function( index, value ) {
           var input_str = "";
             $.each(value.inputs, function( index, value ) {
-              var input_unit_form = `<label for="${value.id}">${value.lable}</label>     <input class="form-control  input-lg"		id="${value.id}" type="text">\n`;
-              input_str += input_unit_form;
-              console.log(input_unit_form);
+              input_str += String.format(input_unit_form,value.id,value.lable);
+              //var input_unit_form = `<label for="${value.id}">${value.lable}</label>     <input class="form-control  input-lg"		id="${value.id}" type="text">\n`;
+              //input_str += input_unit_form;
+              console.log(input_str);
             });
             //console.log(input_str);
+            var str_res = String.format(str_form,index,input_str,value.button.id,value.button.lable);
 
-            var str_form = `	<div class="form-group" id=${index}>
-                ${input_str}
-                <button type="button" id='${value.button.id}' class="btn btn-lg btn-primary">${value.button.lable}</button>
-              </div><br/><br/>`;
+            // var str_form = `	<div class="form-group" id=${index}>
+            //     ${input_str}
+            //     <button type="button" id='${value.button.id}' class="btn btn-lg btn-primary">${value.button.lable}</button>
+            //   </div><br/><br/>`;
 
-      		  console.log(str_form);
-            $(selector).append(str_form);
+      		  console.log(str_res);
+            $(selector).append(str_res);
 
       	});
 
@@ -71,4 +79,21 @@
       });
 
 
+    }
+
+
+    String.format = function() {
+    // The string containing the format items (e.g. "{0}")
+    // will and always has to be the first argument.
+    var theString = arguments[0];
+
+    // start with the second argument (i = 1)
+    for (var i = 1; i < arguments.length; i++) {
+    // "gm" = RegEx options for Global search (more than one instance)
+    // and for Multiline search
+    var regEx = new RegExp("\\{" + (i - 1) + "\\}", "gm");
+    theString = theString.replace(regEx, arguments[i]);
+    }
+
+    return theString;
     }
