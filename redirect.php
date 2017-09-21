12078bdd5e3735c41390fa604350d8ee5e0ef5ee
neo1seok <neo1seok@gmail.com>
2017-09-21 16:13:09 +0900

    update

--- redirect.php
+++ redirect.php
@@ -2,101 +2,143 @@
 $sn= $_REQUEST["sn"];
 
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
-<head>
-<title>Bootstrap Example</title>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet"
-	href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-<script
-	src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
-<script
-	src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 
 </head>
+
 <body>
-  <script 		src="js/util.js" type="text/javascript"></script>
-	<script type="text/javascript">
+  <script src="js/util.js" type="text/javascript"></script>
+  <script type="text/javascript">
+    $(function() {
+      console.log('ready');
+      var sn = '<?php echo $sn; ?>';
+      var jscd = get_naviinfo();
 
-  $(function() {
-    console.log('ready');
-    var sn = '<?php echo $sn; ?>';
-    var jscd =  get_naviinfo();
+      ajax_templtate('giant_se/admin.do',
+        function() {
 
-		ajax_templtate('giant_se/admin.do',
-		function() {
+          return {
+            cmd: 'GET_REDIRECT_INFO',
+            params: {
+              ascii_sn: sn,
+              os: jscd.os
+            }
+          }
 
-			return   {cmd : 'GET_REDIRECT_INFO',params : {
-				ascii_sn:sn,
-        os:jscd.os
-			}	}
+        },
+        function(data) {
+          console.log("rcv:", data);
 
-		},
-		function(data) {
-			console.log("rcv:",data);
-
-			$('#url_img').attr('src',data.params.url_img);
-			//$('#goto_app').attr('href',data.params.intent);
-			$('#description').text(data.params.description);
-
-			window.intent = data.params.intent;
-			window.store = data.params.store;
-			console.log(window.intent);
-			if(window.intent != ''){
-				window.location.href = window.intent;
-				// console.log('goto_app');
-				// alert(jscd.os +" " +window.intent);
-				//
-				// window.location.href = window.intent;
-
-			}
+          $('#url_img').attr('src', data.params.url_img);
+          //$('#goto_app').attr('href',data.params.intent);
+          $('#description').text(data.params.description);
+					$('#org_name').text(data.params.org_name);
 
 
-
-
-		});
-
-		console.log(sn);
-
-		//window.location.href = window.intent;
-
-		$('#goto_app').click(function(){
-			console.log('goto_app');
-			//alert(jscd.os +" " +window.intent);
-			if(jscd.os == 'iOS'){
-				var clickedAt = +new Date;
-
-						naverAppCheckTimer = setTimeout(function() {
-								if (+new Date - clickedAt < 2000){
-										if (window.confirm("앱 최신 버전이 설치되어 있지 않습니다.   \n설치페이지로 이동하시겠습니까?"))
-										{ location.href = window.store; }
-								}
-						}, 1500);
-						//window.location.href = window.intent;
-			}
-			else {
-					window.location.href = window.intent;
-			}
-
-		})
+          window.intent = data.params.intent;
+          window.store = data.params.store;
+          console.log(window.intent);
+          // if(window.intent != ''){
+          // 	window.location.href = window.intent;
+          // 	// console.log('goto_app');
+          // 	// alert(jscd.os +" " +window.intent);
+          // 	//
+          // 	// window.location.href = window.intent;
+          //
+          // }
 
 
 
 
+        });
 
-  });
+      console.log(sn);
+
+      //window.location.href = window.intent;
+
+      $('#goto_app').click(function() {
+        console.log('goto_app');
+        //alert(jscd.os +" " +window.intent);
+        if (jscd.os == 'iOS') {
+          var clickedAt = +new Date;
+
+          naverAppCheckTimer = setTimeout(function() {
+            if (+new Date - clickedAt < 2000) {
+              if (window.confirm("앱 최신 버전이 설치되어 있지 않습니다.   \n설치페이지로 이동하시겠습니까?")) {
+                location.href = window.store;
+              }
+            }
+          }, 1500);
+          //window.location.href = window.intent;
+        } else {
+          window.location.href = window.intent;
+        }
+
+      })
+
+
+
+
+
+    });
   </script>
-<div class="">
-<img src="/img/ictk_logo.png" id = 'url_img' class="img-rounded" >
-</div>
-<div class="well">
-        <p id ='description' >NO COMPANY</p>
-        <h2><a id ='goto_app' href="#">앱으로 이동</a></h2>
+  <div class="container">
+
+  </div>
+<!--
+	<div class="w3-container w3-gray w3-center">
+	  <h1 id="org_name">Header</h1>
+
+	</div>
+
+<div class="w3-container w3-center">
+
+	<p id='description'>The w3-container class can be used to display headers.</p>
+	<div class="">
+		<img src="" id='url_img' alt="Chania">
+	</div>
+	<hr>
+
+</div> -->
+<div class="w3-row w3-border">
+	<div class="w3-center w3-half">
+		  <img class="w3-image" id='url_img' src="img_fjords.jpg" alt="Norway" >
+	</div>
+<div class="w3-container w3-half">
+  <h2 id="org_name">Responsive Images</h2>
+  <p  id='description'>The w3-image class makes the image scale down if it has to, but never scale up to be larger than its original size (<strong>resize the browser window to see the effect</strong>):</p>
+
 </div>
 
+</div>
+<!--
+<div class="w3-row w3-border">
+	<div class="w3-container w3-half">
+	  <img src="" id='url_img' alt="Chania">
+	</div>
+<div class="w3-container w3-half w3-gray">
+  <h2 id="org_name">w3-half</h2>
+  <p id='description'>The w3-half class uses 50% of the parent container.</p>
+	<p></p>
+	<p></p>
+
+</div> -->
+
+<hr>
+<div class=" w3-center">
+<button class="w3-btn w3-green w3-center"  id='goto_app'>앱으로 이동</button>
+</div>
+
+</div>
 
 </body>
